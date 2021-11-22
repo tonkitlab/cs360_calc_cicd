1d2774293dd3b39e9b8207ca327473095676c501
tonkitlab <85208276+tonkitlab@users.noreply.github.com>
2021-11-22 13:57:35 +0700

    Revised host in index.js and add a new Dockerfile

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const Hapi = require('hapi');
 
 //Address and port
-const host = 'localhost';
+const host = '0.0.0.0';
 const port = 3000; 
 
 //Create server
@@ -17,7 +17,7 @@ require('./routes/routes')(server);
 //Start server
 const init = async () => {
     await server.start();
-    console.log("Server up! Port: " + port);
+    console.log(`Running on http://${host}:${port}`);
 }
 
 //Start App
